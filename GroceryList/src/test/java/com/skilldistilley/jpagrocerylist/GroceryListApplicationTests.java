fc4f993ec9a30d4b8ed2fcfde0562dbef0374856
Vinton Lee <vinton.e.lee@gmail.com>
2019-11-09 10:08:31 -0700

    fixed JUNIT testing

--- GroceryList/src/test/java/com/skilldistilley/jpagrocerylist/GroceryListApplicationTests.java
+++ GroceryList/src/test/java/com/skilldistilley/jpagrocerylist/GroceryListApplicationTests.java
@@ -1,13 +1,45 @@
 package com.skilldistilley.jpagrocerylist;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.skilldistilley.jpagrocerylist.dao.GroceryDAO;
 
 @SpringBootTest
 class GroceryListApplicationTests {
 
+	@Autowired
+	private GroceryDAO groceryDao;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
 	@Test
-	void contextLoads() {
+	@Disabled
+	@DisplayName("Test to make sure the test starts up")
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	@DisplayName("Test findById")
+	void test1() {
+		assertEquals("bananas", groceryDao.findById(1).getItem());
 	}
 
 }
+
