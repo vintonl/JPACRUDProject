8c95c80a439c174c07a76623251ff159a6032191
Vinton Lee <vinton.e.lee@gmail.com>
2019-11-08 19:37:06 -0700

    added getGroceries.do

--- GroceryList/src/main/java/com/skilldistilley/jpagrocerylist/controllers/GroceryController.java
+++ GroceryList/src/main/java/com/skilldistilley/jpagrocerylist/controllers/GroceryController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistilley.jpagrocerylist.dao.GroceryDAO;
 import com.skilldistilley.jpagrocerylist.entities.Grocery;
@@ -22,6 +25,19 @@ public class GroceryController {
 		model.addAttribute("groceries", groceries);
 
 		return "index";
+	}
+	
+	@RequestMapping(path = "getGroceries.do", method = RequestMethod.GET)
+	public ModelAndView getFilm(@RequestParam("find") String find) {
+		ModelAndView mv = new ModelAndView();
+
+		List<Grocery> groceries = groceryDao.findByItem(find);
+		// film is unmanaged after it is outside of the transaction that exists in the
+		// DAO
+
+		mv.addObject("groceries", groceries);
+		mv.setViewName("grocery/show");
+		return mv;
 	}
 	
 }
