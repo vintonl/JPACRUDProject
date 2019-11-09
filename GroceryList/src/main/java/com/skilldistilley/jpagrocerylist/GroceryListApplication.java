5c0cbba040332d1373ea9188457795d6be35566f
Vinton Lee <vinton.e.lee@gmail.com>
2019-11-08 18:48:58 -0700

    added @EntityScan

--- GroceryList/src/main/java/com/skilldistilley/jpagrocerylist/GroceryListApplication.java
+++ GroceryList/src/main/java/com/skilldistilley/jpagrocerylist/GroceryListApplication.java
@@ -2,8 +2,10 @@ package com.skilldistilley.jpagrocerylist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.skilldistilley.jpagrocerylist")
 public class GroceryListApplication {
 
 	public static void main(String[] args) {
