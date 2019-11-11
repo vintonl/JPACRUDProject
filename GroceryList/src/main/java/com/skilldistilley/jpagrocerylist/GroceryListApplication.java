0ff8e9b353f025252be8affe37797fbc2049fe5e
Vinton Lee <vinton.e.lee@gmail.com>
2019-11-11 10:00:53 -0700

    making AWS ready

--- GroceryList/src/main/java/com/skilldistilley/jpagrocerylist/GroceryListApplication.java
+++ GroceryList/src/main/java/com/skilldistilley/jpagrocerylist/GroceryListApplication.java
@@ -3,11 +3,17 @@ package com.skilldistilley.jpagrocerylist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistilley.jpagrocerylist")
-public class GroceryListApplication {
+public class GroceryListApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GroceryListApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroceryListApplication.class, args);
